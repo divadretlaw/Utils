a769f8b5160e6d9a41d09014ba9c9117f8b5ea0b
David Walter <hello@davidwalter.at>
2023-01-21 18:15:12 +0100

    Add basic Default App Link detection

--- Sources/SafariServicesUI/URL+Extensions.swift
+++ Sources/SafariServicesUI/URL+Extensions.swift
@@ -10,6 +10,24 @@ import Foundation
 extension URL {
     var supportsSafari: Bool {
         guard let scheme = scheme else { return false }
-        return ["https", "http"].contains(scheme.lowercased())
+        guard ["https", "http"].contains(scheme.lowercased()) else { return false }
+        return !isDefaultAppLink
+    }
+    
+    var isDefaultAppLink: Bool {
+        isAppStore || isMapsLink
+    }
+    
+    private var isAppStore: Bool {
+        guard let host = host?.lowercased() else { return false }
+        guard ["apps.apple.com", "itunes.apple.com"].contains(host) else { return false }
+        guard lastPathComponent.hasPrefix("id") else { return false }
+        return Int(lastPathComponent.dropFirst(2)) != nil
+    }
+    
+    private var isMapsLink: Bool {
+        guard let host = host?.lowercased() else { return false }
+        guard "maps.apple.com" == host else { return false }
+        return query != nil
     }
 }
